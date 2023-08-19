bba5922a9137c105c8951a05394b94805a53cee2
Subhey Sadi <srahman212074@bscse.uiu.ac.bd>
2023-08-19 08:13:15 +0600

    Offline 4 in CSE3812

---
+++ CSE 3712 CN Lab/Term Project/Server.py
@@ -0,0 +1,31 @@
+import socket
+import threading
+
+PORT = 8888
+# Get the host ipv4 address
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    connected = True
+    while connected:
+        msg = conn.recv()
+
+
+def start():
+    server.listen()
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTION] {threading.active_count() - 1}")
+
+
+print("[STARTING] Server is starting...")
+start()
