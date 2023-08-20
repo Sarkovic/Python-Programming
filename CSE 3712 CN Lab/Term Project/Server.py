b179edbf55b55f0d2ef3dea997303fc7556b6c94
Subhey Sadi <srahman212074@bscse.uiu.ac.bd>
2023-08-20 09:40:06 +0600

    CSE 3712 Server

--- CSE 3712 CN Lab/Term Project/Server.py
+++ CSE 3712 CN Lab/Term Project/Server.py
@@ -1,29 +1,46 @@
 import socket
 import threading
 
+HEADER = 64
+FORMAT = 'utf-8'
 PORT = 8888
 # Get the host ipv4 address
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
+DISCONNECT_MESSAGE = '!DISCONNECT'
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
 
+# One client and one server handling
 def handle_client(conn, addr):
     print(f"[NEW CONNECTION] {addr} connected.")
 
     connected = True
     while connected:
-        msg = conn.recv()
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode(FORMAT)
+        if msg == DISCONNECT_MESSAGE:
+            connected = False
+
+        print(f"[{addr}] {msg}")
+
+    conn.close()
 
 
+# Handle new connection
 def start():
     server.listen()
+    print(f'[LISTENING] Server is listening on {SERVER}')
     while True:
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
+        # Number of threads active in this process
+        # The amount of threads represents the number of clients
+        # The start listen thread is always running, so we subtract 1
         print(f"[ACTIVE CONNECTION] {threading.active_count() - 1}")
 
 
