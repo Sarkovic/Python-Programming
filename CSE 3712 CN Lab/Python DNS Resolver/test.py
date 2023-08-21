cb01d9a1e7f4ebf26485f7effead61b8266ee2df
Subhey Sadi <srahman212074@bscse.uiu.ac.bd>
2023-08-22 02:26:30 +0600

    CSE 3712

--- CSE 3712 CN Lab/Python DNS Resolver/test.py
+++ CSE 3712 CN Lab/Python DNS Resolver/test.py
@@ -1,7 +1,6 @@
 import socket
 import struct
 
-
 def resolve_dns(domain_name, dns_server):
     # Create a UDP socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -11,22 +10,37 @@ def resolve_dns(domain_name, dns_server):
     query_id = 12345  # Choose any query ID you like
     query = build_dns_query(domain_name, query_id)
 
-    try:
-        # Send the DNS query to the DNS server
-        client_socket.sendto(query, (dns_server, 53))
+    while True:
+        try:
+            # Send the DNS query to the DNS server
+            client_socket.sendto(query, (dns_server, 53))
 
-        # Receive the DNS response
-        response, server_address = client_socket.recvfrom(1024)
+            # Receive the DNS response
+            response, server_address = client_socket.recvfrom(1024)
 
-        # Parse the DNS response
-        ip_addresses = parse_dns_response(response)
-        return ip_addresses
+            # Parse the DNS response
+            ip_addresses, canonical_name, name_server = parse_dns_response(response)
 
-    except socket.timeout:
-        print("DNS resolution timed out")
-    finally:
-        client_socket.close()
+            if ip_addresses:
+                return ip_addresses
+            elif canonical_name:
+                print(canonical_name)
+                domain_name = canonical_name.decode()
+                query = build_dns_query(domain_name, query_id)
+            elif name_server:
+                print(name_server)
+                # Convert an ip address of 32 bits binary format into string format
+                dns_server = socket.inet_ntoa(name_server)
+                query = build_dns_query(domain_name, query_id)
+            else:
+                break
 
+        except socket.timeout:
+            print("DNS resolution timed out")
+            break
+
+    client_socket.close()
+    return []
 
 def build_dns_query(domain_name, query_id):
     # Build the DNS query message as per the DNS protocol
@@ -48,30 +62,38 @@ def build_dns_query(domain_name, query_id):
     query = header + qname + qtype + qclass
     return query
 
-
 def parse_dns_response(response):
-    # Parse the DNS response to extract IP addresses from the answer section
+    # Parse the DNS response to extract IP addresses, CNAME, and NS records from the answer section
     # This involves reading the header, question section, and answer section
-    # For simplicity, we'll only consider A records in the answer section
 
     header = struct.unpack('!HHHHHH', response[:12])
     qdcount = header[2]
     ancount = header[3]
+    nscount = header[4]
 
     domain_name_end = response.find(b'\x00', 12) + 1
     answer_section = response[domain_name_end + 4:]  # Skip over the question section
 
     ip_addresses = []
+    canonical_name = None
+    name_server = None
 
     for _ in range(ancount):
         name, qtype, qclass, ttl, rdlength = struct.unpack('!HHHLH', answer_section[:12])
         if qtype == 1 and qclass == 1:  # A record and IN class
             ip_address = socket.inet_ntoa(answer_section[12:16])
             ip_addresses.append(ip_address)
+        elif qtype == 5 and qclass == 1:  # CNAME record and IN class
+            canonical_name = answer_section[12:12+rdlength]
         answer_section = answer_section[12 + rdlength:]
 
-    return ip_addresses
+    for _ in range(nscount):
+        name, qtype, qclass, ttl, rdlength = struct.unpack('!HHHLH', answer_section[:12])
+        if qtype == 2 and qclass == 1:  # NS record and IN class
+            name_server = answer_section[12:12+rdlength]
+        answer_section = answer_section[12 + rdlength:]
 
+    return ip_addresses, canonical_name, name_server
 
 if __name__ == "__main__":
     domain_name = input("Enter a domain name: ")
